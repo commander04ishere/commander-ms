fe14d71d2b8330629dcf889f537e17a604364ff5
commander04ishere <artin.golestani1402@gmail.com>
2026-08-10 13:09:36 +0330

    Update config.py

--- commander messager/config.py
+++ commander messager/config.py
@@ -3,17 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = os.path.abspath(os.path.dirname(file))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "dev-secret-change-me"
+    )
 
-SECRET_KEY = os.getenv(
-    "SECRET_KEY",
-    "dev-secret-change-me"
-)
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL"
+    )
 
-SQLALCHEMY_DATABASE_URI = os.getenv(
-    "DATABASE_URL"
-)
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
